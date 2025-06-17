25d9f0907277fb5cc7c30ef24d788a1b4868802b
HenryKim12 <henrykim966@gmail.com>
2025-06-16 18:41:13 -0700

    init spring boot app

---
+++ src/main/java/com/example/bounder/BounderApplication.java
@@ -0,0 +1,13 @@
+package com.example.bounder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BounderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BounderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bounder/BounderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bounder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BounderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
